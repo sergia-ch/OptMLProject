42ff73dbd4099a1a9b424ec6f53eeb938a3cf7dd
Sergei Volodin <sergei.volodin@epfl.ch>
2019-05-30 13:24:22 +0200

    Add runs 1

---
+++ run_all_1.sh
@@ -0,0 +1,11 @@
+python experiment.py --eta 0.1 --rho 0.6 --mu 0 --epochs 20 --train_batch_size 256 &
+python experiment.py --eta 0.1 --rho 0.65 --mu 0 --epochs 20 --train_batch_size 256 &
+python experiment.py --eta 0.1 --rho 0.7 --mu 0 --epochs 20 --train_batch_size 256 &
+python experiment.py --eta 0.1 --rho 0.75 --mu 0 --epochs 20 --train_batch_size 256 &
+python experiment.py --eta 0.1 --rho 0.8 --mu 0 --epochs 20 --train_batch_size 256 &
+python experiment.py --eta 0.1 --rho 0 --mu 0.4 --epochs 20 --train_batch_size 256 &
+python experiment.py --eta 0.1 --rho 0 --mu 0.45 --epochs 20 --train_batch_size 256 &
+python experiment.py --eta 0.1 --rho 0 --mu 0.5 --epochs 20 --train_batch_size 256 &
+python experiment.py --eta 0.1 --rho 0 --mu 0.55 --epochs 20 --train_batch_size 256 &
+python experiment.py --eta 0.1 --rho 0 --mu 0.6 --epochs 20 --train_batch_size 256 &
+
